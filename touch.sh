085ca2d46dc4b7032aa2a78fe5cf75a8608820e2
pavanyendluri588 <pavanyendluri588@gmail.com>
2022-11-30 19:04:11 +0530

    sem5_upload

---
+++ touch.sh
@@ -0,0 +1,9 @@
+git init
+git remote add origin https://github.com/pavanyendluri588/sem5.git
+git remote -v  
+git add . --force
+git commit -m "sem5_upload"
+
+
+git push -u origin master  --force
+
