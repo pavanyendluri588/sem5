4e54cf6cd8fcb9d6af711135b0dcfa0e632a6b50
pavanyendluri588 <pavanyendluri588@gmail.com>
2022-12-01 17:02:10 +0530

    koushik

--- touch.sh
+++ touch.sh
@@ -2,7 +2,7 @@ git init
 git remote add origin https://github.com/pavanyendluri588/sem5.git
 git remote -v  
 git add . --force
-git commit -m "sem5_upload"
+git commit -m "koushik"
 
 
 git push -u origin master  --force
